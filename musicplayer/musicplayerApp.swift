a9f9ce5507ee2530bf6e022de7a2206c9a0d0c9e
Amy Jhou <amycjhou@gmail.com>
2022-06-21 10:57:32 -0400

    Initial Commit

---
+++ musicplayer/musicplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicplayerApp.swift
+//  musicplayer
+//
+//  Created by Amy Jhou on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct musicplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
